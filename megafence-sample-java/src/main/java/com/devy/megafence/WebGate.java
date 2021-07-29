467c562308d505df6426c5328e9b113b9178bc9f
devyang75 <devyang75@gmail.com>
2021-07-29 18:44:21 +0900

    Update WebGate.java

--- megafence-sample-java/src/main/java/com/devy/megafence/WebGate.java
+++ megafence-sample-java/src/main/java/com/devy/megafence/WebGate.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /* 
 * ==============================================================================================
-* 메가펜스 유량제어서비스 Backend Library for JAVA / V.21.1.3
+* 메가펜스 유량제어서비스 Backend Library for JAVA / V.21.1.4
 * 이 라이브러리는 메가펜스 서비스 계약 및 테스트(POC) 고객에게 제공됩니다.
 * 오류조치 및 개선을 목적으로 자유롭게 수정 가능하며 수정된 내용은 반드시 공급처에 통보해야 합니다.
 * 허가된 고객 및 환경 이외의 열람, 복사, 배포, 수정, 실행, 테스트 등 일체의 이용을 금합니다.
@@ -50,7 +50,7 @@ import javax.servlet.http.HttpServletResponse;
 public class WebGate {
 	//private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	String  $WG_VERSION            	= "V.21.1.3";           
+	String  $WG_VERSION            	= "V.21.1.4";           
 	String  $WG_SERVICE_ID        	= "";          				// 할당받은 Service ID
 	String  $WG_GATE_ID            	= "";             			// 사용할 GATE ID
 	int     $WG_MAX_TRY_COUNT      	= 3;                    	// [fixed] failover api retry count
